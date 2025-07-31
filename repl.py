3329f844d8e48b520685a4271488faf92c5fc085
jameswesc <jms.wsc@gmail.com>
2025-07-31 18:04:46 +1000

    move data to data-subset

--- repl.py
+++ repl.py
@@ -1,6 +1,4 @@
 # %%
-from time import time
-
 import geopandas as gpd
 import numpy as np
 import pandas as pd
@@ -15,22 +13,21 @@ pdal  # pyright: ignore
 gpd  # pyright: ignore
 
 # %%
-site_plot_id = "NRO_138_P2"
-start = time()
-pl = (
-    pdal.Reader(f"data/plots/lidar/{site_plot_id}.copc.laz")
-    | pdal.Filter("filters.sort", dimension="HeightAboveGround", order="DESC")
-    | pdal.Filter("filters.litree", min_points=20, min_height=0.5, radius=100)
-    | pdal.Writer(
-        f"data/plots/lidar_litree/{site_plot_id}.copc.laz",
-        type="writers.copc",
-        extra_dims="all",
-    )
-)
+pl = pdal.Reader("data/plots/lidar/AGG_O_07_P5.copc.laz").pipeline()
 pl.execute()
-end = time()
-print(f"Time taken: {end - start:,} seconds")
 # %%
 points = pd.DataFrame(pl.arrays[0])
+points.head()
+
 # %%
-points["ClusterID"]
+points["W"] = points["ReturnNumber"] / points["NumberOfReturns"]
+
+
+# %%
+def p_gap(points, z):
+    return (
+        1
+        - (
+            points[points["HeightAboveGround"] > z]["W"].sum() / points["W"].sum()
+        ).item()
+    )
